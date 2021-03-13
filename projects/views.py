7e907fca2a943e5948f47eae6c6abc2e2d06cc97
Filip Dabkowski <filip.dabkowski@gmail.com>
2021-03-13 16:40:56 +0100

    basic in projects_list and register models

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from .models import Project
+
 
 def projects_list_view(request):
-	return render(request, 'projects/projects_list.html', {})
+	context = {}
+	
+	user_projects = Project.objects.all()
+	context['projects'] = user_projects
+	
+	return render(request, 'projects/projects_list.html', context)
